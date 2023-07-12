bfe5cc4486b878cd4ff2b4ad43ae7cbbad94c48c
Andrew Kravchuk <a.kravchuk@abtollc.com>
2023-07-12 10:48:18 +0300

    updated mvp

--- iframe.js
+++ iframe.js
@@ -1,66 +1,53 @@
 window.onmessage = function(e) {
-  alert('iframe window.onmessage data: '+ JSON.stringify(e.data));
+  console.log('iframe window.onmessage data: '+ JSON.stringify(e.data));
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOMContentLoaded");
+  console.log("iframe DOMContentLoaded");
 
-  document.getElementById("sendIframeEventBtn").addEventListener("click", (e) => {
-    let eventData = document.getElementById('eventData').value;
-    console.log('sendIframeEventBtn', eventData);
-    window.top.postMessage('msg from iframe: ' + eventData, '*');
+  // document.getElementById("sendIframeEventBtn").addEventListener("click", (e) => {
+  //   let eventData = document.getElementById('eventData').value;
+  //   console.log('sendIframeEventBtn', eventData);
+  //   window.top.postMessage('msg from iframe: ' + eventData, '*');
+  // });
+  
+  document.getElementById("finishExerciseBtn").addEventListener("click", (e) => {
+    window.top.postMessage('iframe.close', '*');
   });
 
+  const video = document.getElementById('video');
 
-  var video = document.getElementById('video');
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
+  const errBack = function(e) {
+    console.log('An error has occurred!', e)
+  };
 
-    var errBack = function(e) {
-    	console.log('An error has occurred!', e)
-    };
+  const mediaConfig =  { 
+    video: true,
+    // audio: true
+  };
 
-    // Get access to the camera!
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        // Not adding `{ audio: true }` since we only want video now
-        navigator.mediaDevices.getUserMedia({
-            video: true
-        }).then(function(stream) {
-            video.srcObject=stream;
-            video.play();
-        });
-    }
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia(mediaConfig).then(function(stream) {
+          video.srcObject=stream;
+          video.play();
+      });
+  }
 
-    var mediaConfig =  { video: true };
-
-    //
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia(mediaConfig).then(function(stream) {
-            video.srcObject=stream;
-            video.play();
-        });
-    }
-
-    /* Legacy code below! */
-    else if (navigator.getUserMedia) { // Standard
-        navigator.getUserMedia(mediaConfig, function(stream) {
-            video.src = stream;
-            video.play();
-        }, errBack);
-    } else if (navigator.webkitGetUserMedia) { // WebKit-prefixed
-        navigator.webkitGetUserMedia(mediaConfig, function(stream) {
-            video.src = window.webkitURL.createObjectURL(stream);
-            video.play();
-        }, errBack);
-    } else if (navigator.mozGetUserMedia) { // Mozilla-prefixed
-        navigator.mozGetUserMedia(mediaConfig, function(stream) {
-            video.src = window.URL.createObjectURL(stream);
-            video.play();
-        }, errBack);
-    }
-
-    // Trigger photo take
-    document.getElementById('snap').addEventListener('click', function() {
-      context.drawImage(video, 0, 0, 640, 480);
-    });
+  /* Legacy code below! */
+  else if (navigator.getUserMedia) { // Standard
+      navigator.getUserMedia(mediaConfig, function(stream) {
+          video.src = stream;
+          video.play();
+      }, errBack);
+  } else if (navigator.webkitGetUserMedia) { // WebKit-prefixed
+      navigator.webkitGetUserMedia(mediaConfig, function(stream) {
+          video.src = window.webkitURL.createObjectURL(stream);
+          video.play();
+      }, errBack);
+  } else if (navigator.mozGetUserMedia) { // Mozilla-prefixed
+      navigator.mozGetUserMedia(mediaConfig, function(stream) {
+          video.src = window.URL.createObjectURL(stream);
+          video.play();
+      }, errBack);
+  }
 });
--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-window.onmessage = function(e) {
-  alert('parent window.onmessage data: '+ JSON.stringify(e.data));
-};
-
 let iframe;
+
+window.onmessage = function(e) {
+  console.log('parent window.onmessage data: '+ JSON.stringify(e.data));
+
+  if (e.data === 'iframe.close') {
+    iframe && iframe.remove();
+    document.getElementById("openIframeBtn").style.display = "block";
+  }
+  let msgEl = document.createElement('p');
+  msgEl.innerHTML = `${ Date.now() + ':' + JSON.stringify(e.data)}`;
+  document.getElementById("iframe-messages").appendChild(msgEl);
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("parent DOMContentLoaded");
 
-  document.getElementById("sendIframeEventBtn").addEventListener("click", (e) => {
-    let eventData = document.getElementById('eventData').value;
-    console.log('sendIframeEventBtn', eventData);
-    window.top.postMessage(eventData, '*');
+  // document.getElementById("sendIframeEventBtn").addEventListener("click", (e) => {
+  //   let eventData = document.getElementById('eventData').value;
+  //   console.log('sendIframeEventBtn', eventData);
+  //   window.top.postMessage(eventData, '*');
 
-    iframe?.contentWindow.postMessage('msg from parent: ' + eventData, '*');
-  });
+  //   iframe?.contentWindow.postMessage('msg from parent: ' + eventData, '*');
+  // });
 
   document.getElementById("openIframeBtn").addEventListener("click", (e) => {
     console.log('openIframeBtn');
+    document.getElementById("openIframeBtn").style.display = "none";
+
     iframe = document.createElement('iframe');
+    iframe.allow="autoplay; camera; microphone";
+    // iframe.src = 'iframe.html';
+    iframe.id = 'iframe-video';
     iframe.src = 'https://akravchukabto.github.io/iframe-messaging/iframe.html';
-    document.body.appendChild(iframe);
+    document.getElementById("iframe-place").appendChild(iframe);
   });
 
 });
